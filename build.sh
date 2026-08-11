59a97c448d2c7ae031739eba08d72a620e201371
elmendezz <165435968+elmendezz@users.noreply.github.com>
2026-08-11 15:29:37 -0700

    Actualizar SwiftStore

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p Payload/SwiftStore.app
+
+swiftc -parse-as-library \
+       -sdk $(xcrun --sdk iphoneos --show-sdk-path) \
+       -target arm64-apple-ios15.0 \
+       -emit-executable SideloadApp.swift \
+       -o Payload/SwiftStore.app/SwiftStore
+
+cp Info.plist Payload/SwiftStore.app/Info.plist
+zip -r SwiftStore.ipa Payload
